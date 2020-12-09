938b506508319018a507b256333417c111d9a78b
yanagidahana <hana878787zzz@gmail.com>
2020-12-09 16:40:57 +0900

    必要なカラムを記述して、マイグレーションを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_071451) do
+ActiveRecord::Schema.define(version: 2020_12_09_073949) do
+
+  create_table "book_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "book_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_book_users_on_book_id"
+    t.index ["user_id"], name: "index_book_users_on_user_id"
+  end
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -30,4 +39,6 @@ ActiveRecord::Schema.define(version: 2020_12_09_071451) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "book_users", "books"
+  add_foreign_key "book_users", "users"
 end
