515a4ef6d452ec8b783ba56f7e41ce1f5be36310
yanagidahana <hana878787zzz@gmail.com>
2020-12-09 15:43:29 +0900

    更新成功後にページ遷移が行われるように実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,11 @@ class UsersController < ApplicationController
   end
 
   def update
-    current_user.update(user_params)
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
