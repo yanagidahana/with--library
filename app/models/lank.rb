cbfdba0e31de4238bf29af28755f25257221d4ad
yanagidahana <hana878787zzz@gmail.com>
2020-12-09 19:34:04 +0900

    ActiveHashの作成

---
+++ app/models/lank.rb
@@ -0,0 +1,10 @@
+class Lank < ApplicationRecord
+  self.data = [
+    { id: 0, name: '--' },
+    { id: 1, name: '★' },
+    { id: 2, name: '★ ★' },
+    { id: 3, name: '★ ★ ★' },
+    { id: 4, name: '★ ★ ★ ★' },
+    { id: 5, name: '★ ★ ★ ★ ★' },
+  ]
+end
