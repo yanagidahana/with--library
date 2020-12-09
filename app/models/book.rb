ea605c92f9176bcd3380557ebfe38fc04edc5bf6
yanagidahana <hana878787zzz@gmail.com>
2020-12-09 20:56:48 +0900

    バリデーションを設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,17 @@ class Book < ApplicationRecord
   has_many :book_users
   has_many :users, through: :book_users
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :lank
+  belongs_to_active_hash :read
+  belongs_to_active_hash :category
+
+  #空の投稿を保存できないようにする
+  validates :title, :impressions, :lank_id, :category_id, :author, :read_id presence: true
+
+#ジャンルの選択が「--」の時は保存できないようにする
+validates :lank_id, :category_id, :read_id numericality
+
  # def change
   #  create_table :books do |t|
    #   t.string :name, null: false
