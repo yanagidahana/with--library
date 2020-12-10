13a3d368c66aabe2c4185f86ffef35ac2616506e
yanagidahana <hana878787zzz@gmail.com>
2020-12-10 12:43:11 +0900

    マイグレーションを編集

--- db/migrate/20201210024408_create_outputs.rb
+++ db/migrate/20201210024408_create_outputs.rb
@@ -2,9 +2,10 @@ class CreateOutputs < ActiveRecord::Migration[6.0]
   def change
     create_table :outputs do |t|
       t.string  :content
-      t.references :room, foreign_key: true
+      t.references :library, foreign_key: true
       t.references :user, foreign_key: true  
       t.timestamps
     end
   end
 end
+
