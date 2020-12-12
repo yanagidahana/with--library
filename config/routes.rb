1d7ba9d1c99d729281f1da19712109ec8f7884b5
yanagidahana <hana878787zzz@gmail.com>
2020-12-12 17:34:47 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "outputs#index"
   resources :users, only: [:edit, :update]
-  resources :libraries, only: [:new, :create]
-  
+  resources :libraries, only: [:new, :create] do
+    resources :outputs, only: [:index, :create]
+  end
 end
