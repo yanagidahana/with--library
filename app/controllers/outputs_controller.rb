014ee7cbec17d04072c75a1e8ca76044e80d8ab4
yanagidahana <hana878787zzz@gmail.com>
2020-12-17 11:37:38 +0900

    アウトプット送信機能の実装

--- app/controllers/outputs_controller.rb
+++ app/controllers/outputs_controller.rb
@@ -1,4 +1,22 @@
 class OutputsController < ApplicationController
   def index
+    @output = Output.new
+    @library = Library.find(params[:library_id])
+  end
+
+  def create
+    @library = Library.find(params[:library_id])
+    @output = @library.outputs.new(output_params)
+    if @output.save
+      redirect_to library_outputs_path(@library)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def output_params
+    params.require(:output).permit(:content).merge(user_id: current_user.id)
   end
 end
