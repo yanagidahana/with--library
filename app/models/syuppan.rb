557dda74fdcc0a4196113efa37299c6022a668f6
yanagidahana <hana878787zzz@gmail.com>
2020-12-24 07:37:52 +0900

    ActiveHash::Baseの設定

---
+++ app/models/syuppan.rb
@@ -0,0 +1,18 @@
+class Syuppan < ApplicationRecord
+  self.data = [
+    {id: 0, name: '--'},
+    {id: 1, name: '自宅'},
+    {id: 2, name: 'カフェ（チェーン店）'},
+    {id: 3, name: 'カフェ（個人店）'},
+    {id: 4, name: '喫茶店'},
+    {id: 5, name: 'ネットカフェ'},
+    {id: 6, name: '図書館'},
+    {id: 7, name: '電車.バスなどの交通機関'},
+    {id: 8, name: '公園'},
+    {id: 9, name: 'ファミレス'},
+    {id: 10, name: 'ビジネスホテル'},
+    {id: 11, name: '職場'},
+    {id: 12, name: 'その他'},
+    ]
+
+end
