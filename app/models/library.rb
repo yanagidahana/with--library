97c597a440c96847e862e9e756150b9f60b57f01
yanagidahana <hana878787zzz@gmail.com>
2020-12-24 08:06:57 +0900

    バリデーションを設定

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,9 +1,12 @@
 class Library < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :syuppan, :lank, :read
+
   belongs_to :user
   has_many   :outputs, dependent: :destroy  
 
   validates :title, :impressions, :author, :syuppan, presence: true
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :syuppan, :lank, :read
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id, :lank_id, :read, numericality: { other_than: 0 } 
 end
