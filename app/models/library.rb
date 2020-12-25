421d3a3bf273347c74004d96da35756e62d0f620
yanagidahana <hana878787zzz@gmail.com>
2020-12-25 12:37:05 +0900

    アクティブハッシュバリデーション消去

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,12 +1,10 @@
 class Library < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :syuppan, :lank, :read
+# belongs_to :syuppan, :lank, :read
 
   belongs_to :user
   has_many   :outputs, dependent: :destroy  
 
   validates :title, :impressions, :author, :syuppan, presence: true
 
-  #ジャンルの選択が「--」の時は保存できないようにする
-  validates :category_id, :lank_id, :read_id, numericality: { other_than: 0 } 
 end
