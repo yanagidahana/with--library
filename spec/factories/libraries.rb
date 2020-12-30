6639a63f0b9f515bd6368ba102d8eaf562af3202
yanagidahana <hana878787zzz@gmail.com>
2020-12-30 22:44:18 +0900

    libraryをFactoryBotで指定

--- spec/factories/libraries.rb
+++ spec/factories/libraries.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
-  factory :room do
-    name {Faker::Team.name}
+  factory :library do
+    title {"タイトル"}
+    impressions {"アウトプット"}
+    category_id {2}
+    lank_id {2}
+    author {"著者"}
+    syuppan {"文庫"}
+    read_id {2}
+    association :user
   end
 end
+
+
